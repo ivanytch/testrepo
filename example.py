498926bee5ce621551549e3391f3633bffb82485
ivanytch <ivanytch@gmail.com>
2023-01-29 14:26:00 -0500

    Add files via upload
    Upload 2nd python file

---
+++ example.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Comment
+print ("Hello") # Comment ::
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+
+
+for x in range(5):
+    print (x+1.)
+
+
+"""
+Multy-line
+comment
+"""
